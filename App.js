974aba40c1acd5cc11f04eef6d9b7c9e1b3708c6
Zaiba Attar <74246426+Zaiba-Attar@users.noreply.github.com>
2021-11-23 16:17:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+export default class UserList extends React.Component {   
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: []
+    };
+  }
+  UserList(){
+    return $.getJSON('https://jsonkeeper.com/b/07NH')
+    .then(function(data) {
+      return data.results;
+    });
+  }
+  render() {
+    this.UserList().then(function(res){
+      this.state = {person: res};
+    });
+    return (
+      <div>
+      <div id="layout-content" className="layout-content-wrapper">
+        <div className="panel-list">
+          {this.state.person.map((item, i) =>{
+            return(
+              <h1>{item.id.first}</h1>
+              <span>{item.name}, {item.email}, {item.aboutMe}, {item.location}, {item.department},
+                    {item.gender}, {item.joiningDate}
+              </span>
+            )
+          })}
+        <div>
+      </div>
+      </div>
+    )
+  }
+}
